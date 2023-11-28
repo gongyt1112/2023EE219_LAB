dc6b18d1a71520936bc4889e5684753524fa3b54
gongyt <gongyt@shanghaitech.edu.cn>
2023-11-28 17:27:45 +0800

    *

--- projects/lab3/build_test/emu-compile/main.d
+++
@@ -1,9 +0,0 @@
-main.o: /home/docker/workspace/EE219-ICS2023/projects/lab3/csrc/main.cpp \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h Vtop.h \
- /usr/local/share/verilator/include/verilated_vcd_c.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_trace.h \
- /usr/local/share/verilator/include/verilated_trace_defs.h
